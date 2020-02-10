9af9c6d041ea0bbd262372199a4fe07a64834624
Luís Pinto 1180965 <1180965@isep.ipp.pt>
2020-02-10 23:26:04 +0000

    feat(db): Schema created in Go

--- db/sqlite.go
+++ db/sqlite.go
@@ -3,6 +3,7 @@ package db
 import (
 	"database/sql"
 	"fmt"
+	"log"
 
 	"../files"
 )
@@ -22,6 +23,10 @@ func GetDB() (*sql.DB, error) {
 		var err error
 		currentConn, err = sql.Open("sqlite3", files.GetSettingsPath()+FileName)
 
+		if err != nil {
+			log.Fatalln("Unable to use the DB! " + err)
+		}
+
 		// Test query to check if the DB has the expected tables
 		_, err = currentConn.Exec("SELECT id FROM moodles")
 		if err != nil {
@@ -37,24 +42,80 @@ func GetDB() (*sql.DB, error) {
 func populateDB() (err error) {
 	statement, err := currentConn.Prepare(`
 		CREATE TABLE IF NOT EXISTS moodles (
-			id INTEGER PRIMARY KEY
-			           AUTOINCREMENT,
-			url TEXT NOT NULL
-			         CHECK ((url GLOB 'http://*/') OR (url GLOB 'https://*/')),
-			username TEXT NOT NULL
-			              CHECK (LENGTH(username) > 0),
-			password TEXT NOT NULL
-			              CHECK (LENGTH(password) > 0),
-			wstoken TEXT,
-			location TEXT NOT NULL
-			              CHECK (LENGTH(location) > 0),
-			sitename TEXT,
-			firstname TEXT,
-			lastname TEXT,
-			lang TEXT,
-			userid TEXT,
-			userpictureurl TEXT,
-			UNIQUE(url, username)
+		    id INTEGER PRIMARY KEY
+		               AUTOINCREMENT,
+		    url TEXT NOT NULL
+		             CHECK ((url GLOB 'http://*/') OR (url GLOB 'https://*/')),
+		    username TEXT NOT NULL
+		                  CHECK (LENGTH(username) > 0),
+		    password TEXT NOT NULL
+		                  CHECK (LENGTH(password) > 0),
+		    wstoken TEXT,
+		    location TEXT NOT NULL
+		                  CHECK (LENGTH(location) > 0),
+		    sitename TEXT,
+		    firstname TEXT,
+		    lastname TEXT,
+		    lang TEXT,
+		    userid TEXT,
+		    userpictureurl TEXT,
+		    courseshash TEXT,
+		    unhandlednotifications INTEGER,
+		    UNIQUE(url, username)
+		);
+		CREATE TABLE IF NOT EXISTS courses (
+		    moodleid INTEGER UNIQUE,
+		    id INTEGER PRIMARY KEY AUTOINCREMENT,
+		    shortname TEXT NOT NULL,
+		    fullname TEXT NOT NULL,
+		    summary TEXT NOT NULL,
+		    downloaded INTEGER NOT NULL,
+		    showgrades INTEGER NOT NULL,
+		    previoushash TEXT NOT NULL,
+		    unhandlednotifications INTEGER,
+		    newcourse INTEGER,
+		    deletedcourse INTEGER,
+		    FOREIGN KEY (moodleid) REFERENCES moodles(id)
+		);
+		CREATE TABLE IF NOT EXISTS sections (
+		    courseid INTEGER UNIQUE,
+		    id INTEGER PRIMARY KEY AUTOINCREMENT,
+		    name TEXT NOT NULL,
+		    summary TEXT NOT NULL,
+		    FOREIGN KEY (courseid) REFERENCES courses(id)
+		);
+		CREATE TABLE IF NOT EXISTS modules (
+		    sectionid INTEGER UNIQUE,
+		    id INTEGER PRIMARY KEY AUTOINCREMENT,
+		    name TEXT NOT NULL,
+		    modname TEXT NOT NULL,
+		    FOREIGN KEY (sectionid) REFERENCES sections(id)
+		);
+		CREATE TABLE IF NOT EXISTS contents (
+		    moduleid INTEGER UNIQUE,
+		    id INTEGER PRIMARY KEY AUTOINCREMENT,
+		    indexinmodule INTEGER,
+		    type TEXT NOT NULL,
+		    filename TEXT NOT NULL,
+		    filesize INTEGER NOT NULL,
+		    fileurl TEXT NOT NULL,
+		    timecreated INTEGER NOT NULL,
+		    timemodified INTEGER NOT NULL,
+		    author TEXT NOT NULL,
+		    license TEXT NOT NULL,
+		    FOREIGN KEY (moduleid) REFERENCES modules(id)
+		);
+		CREATE TABLE IF NOT EXISTS unhandled_moodles (
+		    moodleid INTEGER,
+		    courseid INTEGER,
+		    FOREIGN KEY (moodleid) REFERENCES moodles(id),
+		    FOREIGN KEY (courseid) REFERENCES courses(id)
+		);
+		CREATE TABLE IF NOT EXISTS unhandled_courses (
+		    courseid INTEGER,
+		    contentid INTEGER,
+		    FOREIGN KEY (courseid) REFERENCES courses(id),
+		    FOREIGN KEY (contentid) REFERENCES contents(id)
 		);`)
 	_, err = statement.Exec()
 	statement.Close()
