030e1b6364b76199ab14af99da20c5ee15c7d796
Luís Pinto 1180965 <1180965@isep.ipp.pt>
2020-02-11 17:03:22 +0000

    feat(db): DB changes

--- db/sqlite.go
+++ db/sqlite.go
@@ -24,7 +24,7 @@ func GetDB() (*sql.DB, error) {
 		currentConn, err = sql.Open("sqlite3", files.GetSettingsPath()+FileName)
 
 		if err != nil {
-			log.Fatalln("Unable to use the DB! " + err)
+			log.Fatalln("Unable to use the DB! " + err.Error())
 		}
 
 		// Test query to check if the DB has the expected tables
@@ -32,6 +32,7 @@ func GetDB() (*sql.DB, error) {
 		if err != nil {
 			// Populate the DB
 			err = populateDB()
+			fmt.Println(err)
 		}
 		return currentConn, err
 	}
@@ -39,8 +40,8 @@ func GetDB() (*sql.DB, error) {
 }
 
 // populateDB creates the required tables for the application to work
-func populateDB() (err error) {
-	statement, err := currentConn.Prepare(`
+func populateDB() error {
+	tables := [7]string{`
 		CREATE TABLE IF NOT EXISTS moodles (
 		    id INTEGER PRIMARY KEY
 		               AUTOINCREMENT,
@@ -59,41 +60,48 @@ func populateDB() (err error) {
 		    lang TEXT,
 		    userid TEXT,
 		    userpictureurl TEXT,
-		    courseshash TEXT,
-		    unhandlednotifications INTEGER,
+		    previoushash TEXT NOT NULL
+		                      DEFAULT 'idk',
+		    unhandlednotifications INTEGER NOT NULL
+		                                   DEFAULT 0,
 		    UNIQUE(url, username)
-		);
+		)`, `
 		CREATE TABLE IF NOT EXISTS courses (
-		    moodleid INTEGER UNIQUE,
-		    id INTEGER PRIMARY KEY AUTOINCREMENT,
+		    moodleid INTEGER,
+		    id INTEGER,
 		    shortname TEXT NOT NULL,
 		    fullname TEXT NOT NULL,
 		    summary TEXT NOT NULL,
 		    downloaded INTEGER NOT NULL,
 		    showgrades INTEGER NOT NULL,
-		    previoushash TEXT NOT NULL,
-		    unhandlednotifications INTEGER,
+		    previoushash TEXT NOT NULL
+		                      DEFAULT 'idk',
+		    unhandlednotifications INTEGER NOT NULL
+		                                   DEFAULT 0,
 		    newcourse INTEGER,
 		    deletedcourse INTEGER,
-		    FOREIGN KEY (moodleid) REFERENCES moodles(id)
-		);
+		    FOREIGN KEY (moodleid) REFERENCES moodles(id),
+			  PRIMARY KEY (moodleid, id)
+		)`, `
 		CREATE TABLE IF NOT EXISTS sections (
-		    courseid INTEGER UNIQUE,
-		    id INTEGER PRIMARY KEY AUTOINCREMENT,
+		    courseid INTEGER,
+		    id INTEGER,
 		    name TEXT NOT NULL,
 		    summary TEXT NOT NULL,
-		    FOREIGN KEY (courseid) REFERENCES courses(id)
-		);
+		    FOREIGN KEY (courseid) REFERENCES courses(id),
+				PRIMARY KEY (courseid, id)
+		)`, `
 		CREATE TABLE IF NOT EXISTS modules (
-		    sectionid INTEGER UNIQUE,
-		    id INTEGER PRIMARY KEY AUTOINCREMENT,
+		    sectionid INTEGER,
+		    id INTEGER,
 		    name TEXT NOT NULL,
 		    modname TEXT NOT NULL,
-		    FOREIGN KEY (sectionid) REFERENCES sections(id)
-		);
+		    FOREIGN KEY (sectionid) REFERENCES sections(id),
+				PRIMARY KEY (sectionid, id)
+		)`, `
 		CREATE TABLE IF NOT EXISTS contents (
-		    moduleid INTEGER UNIQUE,
-		    id INTEGER PRIMARY KEY AUTOINCREMENT,
+		    moduleid INTEGER,
+		    id INTEGER,
 		    indexinmodule INTEGER,
 		    type TEXT NOT NULL,
 		    filename TEXT NOT NULL,
@@ -103,21 +111,40 @@ func populateDB() (err error) {
 		    timemodified INTEGER NOT NULL,
 		    author TEXT NOT NULL,
 		    license TEXT NOT NULL,
-		    FOREIGN KEY (moduleid) REFERENCES modules(id)
-		);
+		    FOREIGN KEY (moduleid) REFERENCES modules(id),
+				PRIMARY KEY (moduleid, id)
+		)`, `
 		CREATE TABLE IF NOT EXISTS unhandled_moodles (
 		    moodleid INTEGER,
 		    courseid INTEGER,
 		    FOREIGN KEY (moodleid) REFERENCES moodles(id),
 		    FOREIGN KEY (courseid) REFERENCES courses(id)
-		);
+		)`, `
 		CREATE TABLE IF NOT EXISTS unhandled_courses (
 		    courseid INTEGER,
 		    contentid INTEGER,
 		    FOREIGN KEY (courseid) REFERENCES courses(id),
 		    FOREIGN KEY (contentid) REFERENCES contents(id)
-		);`)
-	_, err = statement.Exec()
-	statement.Close()
-	return
+		)`}
+
+	// Creates all the tables (https://stackoverflow.com/a/27741584/7269000)
+	txn, err := currentConn.Begin()
+
+	if err != nil {
+		return err
+	}
+
+	defer txn.Rollback()
+
+	for _, q := range tables {
+		// Execute the query in the transaction.
+		_, err := txn.Exec(q)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	// Commit the transaction.
+	return txn.Commit()
 }
